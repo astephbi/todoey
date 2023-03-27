5d74037c25cecddbfc88a4ee04f45d4aef2c87da
Alex <astephbi@gmail.com>
2023-03-27 20:17:37 +0000

    todoey design finished

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -2,14 +2,29 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('New task'),
+      title: Text(
+        'New task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
       trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+        value: isChecked,
+        onChanged: (newValue) {
+          setState(() {
+            isChecked = newValue!;
+          });
+        },
       ),
     );
   }
